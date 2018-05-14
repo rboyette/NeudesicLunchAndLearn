08900081bfe5cc8f2bad58960c60878aa936b5eb
Rachelle Boyette <Rboyette13@eagles.ewu.edu>
2018-05-14 08:45:26 -0700

    Updated to include test project.

---
+++ Animals/Animals.Test/AnimalTest.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Animals;
+
+namespace Animals.Test
+{
+    [TestFixture] 
+    public class AnimalTest
+    {
+        [Test]
+        public void TestCatGreeting()
+        {
+            //arrange
+            var expected = "Meow";
+            var cat = new Cat();
+            //act
+
+            var actual = cat.Greet();
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCatHasFourLegs()
+        {
+            var expected = true;
+            var cat = new Cat();
+
+            
+            var actual = cat.HasFourLegs();
+
+            Assert.AreEqual(expected, actual);
+            //OR
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void TestCatWhoAmI()
+        {
+            var expected = "I am a cat";
+            var cat = new Cat();
+
+            var actual = cat.WhoAmI();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- Animals/Animals/Cat.cs
+++ Animals/Animals/Cat.cs
@@ -8,6 +8,7 @@ namespace Animals
 {
     public class Cat : Animal
     {
+       
         public string Greet()
         {
             return "Meow";
